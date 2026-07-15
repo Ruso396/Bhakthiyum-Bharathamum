95938522580b54679da8e7d1e0e982f8b94895ad
Ruso396 <kalamruso2004@gmail.com>
2026-07-15 16:03:27 +0530

    admin

--- src/services/api.ts
+++ src/services/api.ts
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const API_BASE =
   import.meta.env.VITE_API_URL ||
-  'http://localhost/bakthiyumbharathamum';
+  'http://localhost/bhakthiyumbharathamum';
   
 const api = axios.create({
   baseURL: API_BASE,
